527e92b2787d1d9a53b7c021c7f16f00f7b25ebc
Abhinav Arora <abhinavarora310@gmail.com>
2025-03-29 23:04:00 +0530

    webhook routes integrated

---
+++ apps/backend/Routes/Webhook.ts
@@ -0,0 +1,53 @@
+import express from "express";
+const router = express.Router();
+import { PrismaClient } from "@prisma/client";  
+const prismaClient = new PrismaClient();    
+
+router.post("/train", async(req , res)=>{
+   console.log(req.body);
+   const requestId = req.body.request_id; 
+   const response = await prismaClient.model.update({   
+    //@ts-ignore    
+        where:{
+            falAiRequestId:requestId,
+        },
+        data:{
+            //@ts-ignore
+            trainingStatus:"Generated", 
+            tensorPath:req.body.tensor_path,    
+    }})
+    res.status(200).json({
+        success:true,
+        message:"WEBHOOK RECEIVED SUCCESSFULLY !!"
+    })
+    
+    return;
+})
+
+
+router.post("/image" , async(req , res)=>{
+    console.log(req.body);
+    const requestId = req.body.request_id; 
+    await prismaClient.outputImages.update({
+            //@ts-ignore    
+            where:{
+                falAiRequestId:requestId,
+            },
+            data:{
+                //@ts-ignore
+                status:"Generated", 
+                imageUrl:req.body.image_url
+        }}) 
+    
+    res.status(200).json({
+            success:true,
+            message:"WEBHOOK RECEIVED SUCCESSFULLY !!"
+    })
+    return
+    
+})
+
+
+export default router;
+
+
--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -3,6 +3,9 @@ import cors from "cors"
 import packRoutes from "./Routes/Pack"
 import ImageRoutes from "./Routes/Image"
 import AIRoutes  from "./Routes/Ai"
+import WebhookRoutes from "./Routes/Webhook"
+
+
 const app = express();
 
 app.use(cors())
@@ -12,23 +15,7 @@ app.listen(4000 , ()=>{
     console.log("Server listening at port 4000 !");
 })
 
-app.post("/fal-ai/webhook/train" , (req , res)=>{
-   console.log(req.body);
-    res.status(200).json({
-        success:true,
-        message:"WEBHOOK RECEIVED SUCCESSFULLY !!"
-    })  
-})
-
-
-app.post("/fal-ai/webhook/image" , (req , res)=>{
-    console.log(req.body);
-    res.status(200).json({
-        success:true,
-        message:"WEBHOOK RECEIVED SUCCESSFULLY !!"
-    })
-})
-app.get("/" , (req , res)=>{
+app.get("/" , async (req , res)=>{
     res.status(200).json({
         success:true,
         message:"You landed on a test route !"
@@ -40,5 +27,6 @@ app.get("/" , (req , res)=>{
 app.use("/api/v1/pack" , packRoutes);
 app.use("/api/v1/image" , ImageRoutes);
 app.use("/api/v1/ai" , AIRoutes);
+app.use("/fal-ai/webhook" , WebhookRoutes);
 
 
